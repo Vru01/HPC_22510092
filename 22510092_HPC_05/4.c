b5cf0c14ff7e3f37625d2a76134407b22dac2f9c
Vru01 <vrushalisangale16@gmail.com>
2025-08-18 14:11:38 +0530

    A small change.

--- 22510092_HPC_05/4.c
+++ 22510092_HPC_05/4.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <omp.h>
 
-#define N 1000000
+#define N 10   // keep small for demonstration
 
 int main() {
     static int arr[N], prefix[N];
     int i;
 
-    // Initialize array
+    // Initialize array with 1,2,3,4,5,...
     for (i = 0; i < N; i++)
-        arr[i] = 1; 
+        arr[i] = i + 1;  
 
     double start = omp_get_wtime();
 
@@ -18,11 +18,19 @@ int main() {
     {
         #pragma omp for
         for (i = 1; i < N; i++) {
+            //  This has a dependency so OpenMP parallelization here may cause race conditions.
             prefix[i] = prefix[i - 1] + arr[i]; 
         }
     }
 
     double end = omp_get_wtime();
+
+    printf("Prefix sum result:\n");
+    for (i = 0; i < N; i++) {
+        printf("%d ", prefix[i]);
+    }
+    printf("\n");
+
     printf("Prefix sum done in %f seconds\n", end - start);
     return 0;
 }
